797fda0b5ab1100383f621911b3a7e8fde1d363f
OYT <xoem00@gmail.com>
2023-01-31 17:37:17 +0900

    Feat: backend init Spring boot

---
+++ TokGit/src/main/java/com/sofmo/TokGit/TokGitApplication.java
@@ -0,0 +1,13 @@
+package com.sofmo.TokGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokGitApplication.class, args);
+	}
+
+}
