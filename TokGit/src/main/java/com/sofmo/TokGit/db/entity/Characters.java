f7baf579f7491cdf132ef27f89da78a9445cde36
xordbs <xoem00@gmail.com>
2023-02-06 21:36:13 +0900

    feat:jpa database table create

---
+++ TokGit/src/main/java/com/sofmo/TokGit/db/entity/Characters.java
@@ -0,0 +1,33 @@
+package com.sofmo.TokGit.db.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@DynamicInsert
+public class Characters {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "char_id")
+    private Integer charId;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "total_score")
+    private Integer totalScore;
+
+    @Column(name = "char_level")
+    private Integer charLevel;
+
+
+}
--- TokGit/src/main/java/com/sofmo/TokGit/db/entity/User.java
+++ TokGit/src/main/java/com/sofmo/TokGit/db/entity/User.java
@@ -7,6 +7,8 @@ import org.hibernate.annotations.DynamicInsert;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,13 +16,23 @@ import javax.persistence.Id;
 @DynamicInsert
 public class User {
     @Id
-    @Column(name = "user_id", length = 255)
+    @Column(name = "user_id")
     private String userId; // Email or github id
 
-    @Column(name = "user_name", length = 255)
+    @Column(name = "user_name")
     private String userName; // user name
 
-    @Column(name = "user_nick", length = 255)
+    @Column(name = "user_nick")
     private String nickName; // user nickname
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt; // 생성일
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt; // 수정일
+
+    @OneToOne(mappedBy = "user")
+    private Characters character;
+
 
 }
