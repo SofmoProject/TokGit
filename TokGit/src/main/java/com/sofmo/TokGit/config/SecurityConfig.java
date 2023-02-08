7669e803406a561db9cd22caf0f532799fd970bc
xordbs <xoem00@gmail.com>
2023-02-09 00:17:26 +0900

    feat: Oauth2.0 SecurityConfig setting init

---
+++ TokGit/src/main/java/com/sofmo/TokGit/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.sofmo.TokGit.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+}
